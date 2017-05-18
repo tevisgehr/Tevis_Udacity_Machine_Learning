1e40ad096d194669136e8991c0b1301e8e925ead
Tevis Gehr <tevis.gehr@gmail.com>
2017-05-18 11:57:11 -0600

    Finished clustering exercise.

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -48,8 +48,9 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -58,14 +59,15 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
+for f1, f2, _ in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
+#print(finance_features)
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-
-
+from sklearn.cluster import KMeans
+pred = KMeans(n_clusters=2, random_state=0).fit_predict(finance_features)
 
 
 ### rename the "name" parameter when you change the number of features
@@ -74,3 +76,21 @@ try:
     Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
+print(len(data_dict))
+
+ex_stock_list = []
+for name in data_dict:
+    if data_dict[name]['exercised_stock_options'] <> 'NaN':
+        ex_stock_list.append(data_dict[name]['exercised_stock_options'])
+print(len(ex_stock_list))  
+print(max(ex_stock_list))
+print(min(ex_stock_list))  
+    
+salary_list = []
+for name in data_dict:
+    if data_dict[name]['salary'] <> 'NaN':
+        salary_list.append(data_dict[name]['salary'])
+print(len(salary_list))  
+print(max(salary_list))
+print(min(salary_list))
+    
